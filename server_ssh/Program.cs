620fe17dd8afa1ed8310a30311e69939b5c1410b
nefir2 <vadim.ahtamov@gmail.com>
2023-05-26 14:57:49 +0300

    some console comand executer

--- server_ssh/Program.cs
+++ server_ssh/Program.cs
@@ -1,28 +1,28 @@
 ﻿using System;
-using Renci.SshNet;  
-
+using Renci.SshNet;
 namespace server_ssh
 {
 	internal static class Program
 	{
 		private static void Main()
 		{
-			ConnectionInfo conInfo = new ConnectionInfo(
-				"localhost",
-				2222,
-				"buntafuji",
-				new PasswordAuthenticationMethod("buntafuji", "12345678")
-			);
-			SshClient ssh = new SshClient(conInfo);
+			using SshClient ssh = new(new("localhost", 2222, "buntafuji", new PasswordAuthenticationMethod("buntafuji", "12345678")));
 			ssh.Connect();
-			SshCommand command = ssh.RunCommand("pwd; ls -lsa"); //вывод текущего пути к текущей папке;
-																 //вывод содержимого папки
-
-			//если в Linux команда возвращает 0 - команда успешно выполнена.
-			if (command.ExitStatus == 0) Console.WriteLine($"result:\n{command.Result}"); 
-			else Console.WriteLine($"error:\n\t{command.Error}");
+			while (true)
+			{
+				string cd = Console.ReadLine() ?? "";
+				if (cd == "" || cd == string.Empty) continue;
+				if (cd.Equals("exit", StringComparison.OrdinalIgnoreCase)) break;
+				if (cd.ToLower() == "clear")
+				{
+					Console.Clear();
+					continue;
+				}
+				SshCommand command = ssh.RunCommand(cd);
+				if (command.ExitStatus == 0) Console.WriteLine($"{command.Result}");
+				else Console.WriteLine($"error:\n\t{command.Error}");
+			}
 			ssh.Disconnect();
-			ssh.Dispose();
 		}
 	}
 }
