4e1f53afabfe7faf8a2f9f8ee1d19e5ec975b3b5
nefir2 <vadim.ahtamov@gmail.com>
2023-05-26 14:07:27 +0300

    Добавьте файлы проекта.

---
+++ server_ssh/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Renci.SshNet;  
+
+namespace server_ssh
+{
+	internal static class Program
+	{
+		private static void Main()
+		{
+			ConnectionInfo conInfo = new ConnectionInfo(
+				"localhost",
+				2222,
+				"buntafuji",
+				new PasswordAuthenticationMethod("buntafuji", "12345678")
+			);
+			SshClient ssh = new SshClient(conInfo);
+			ssh.Connect();
+			SshCommand command = ssh.RunCommand("pwd; ls -lsa"); //вывод текущей пути к текущей папке;
+																 //вывод содержимого папки
+
+			//если в Linux команда возвращает 0 - команда успешно выполнена.
+			if (command.ExitStatus == 0) Console.WriteLine($"result:\n{command.Result}"); 
+			else Console.WriteLine($"error:\n\t{command.Error}");
+			ssh.Disconnect();
+			ssh.Dispose();
+		}
+	}
+}
