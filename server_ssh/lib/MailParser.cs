f91afdb6dca4ea54e9248a61a474e648ec3b7989
nefir2 <vadim.ahtamov@gmail.com>
2023-05-26 15:32:50 +0300

    idk some tryings

---
+++ server_ssh/lib/MailParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server_ssh.lib
+{
+	internal class MailParser
+	{
+		public void Parse(string pathOfMailFile)
+		{
+			using (FileStream sf = new FileStream(pathOfMailFile, FileMode.Open))
+			{
+
+			}
+		}
+	}
+}
